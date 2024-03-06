5f91c250721b551ac0494679630eb0414096bc65
Allan Rodriguez <0x.allan@gmail.com>
2024-03-06 00:36:33 -0600

    feat: add loading refresh

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onBeforeMount } from 'vue'
+import { ref, computed, onBeforeMount, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
 import { useAuthStore } from '@/stores/auth'
@@ -8,6 +8,7 @@ import { useGeneralStore } from '@/stores/general'
 import { HomeView, LoginView } from '@/router'
 
 import AppLayout from '@/components/app/AppLayout.vue'
+import AppLoading from '@/components/app/AppLoading.vue'
 
 const router = useRouter()
 const route = useRoute()
@@ -15,22 +16,29 @@ const route = useRoute()
 const general = useGeneralStore()
 const auth = useAuthStore()
 
+const loading = ref(true)
 const isLogin = computed(() => route.name === LoginView.name)
 
 onBeforeMount(async () => {
+  loading.value = true
+
   await auth.init()
   await general.init()
 
   if (isLogin.value && auth.token) {
     router.replace(HomeView)
   }
+
+  loading.value = false
 })
 </script>
 
 <template>
-  <AppLayout v-if="!isLogin">
-    <RouterView />
-  </AppLayout>
+  <AppLoading :loading="loading">
+    <AppLayout v-if="!isLogin">
+      <RouterView />
+    </AppLayout>
 
-  <RouterView v-else />
+    <RouterView v-else />
+  </AppLoading>
 </template>
