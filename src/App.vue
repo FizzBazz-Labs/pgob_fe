7b567ba5febbcc449209e63c61c69dd94b536e84
Allan Rodriguez <0x.allan@gmail.com>
2024-06-13 21:50:36 -0600

    feat: add can use layout

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { useRoute, useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 import { useGeneralStore } from '@/stores/general'
 
-import { HomeView, LoginView, UnavailableSiteView } from '@/router'
+import { HomeView, LoginView, UnavailableSiteView, NotFoundView } from '@/router'
 
 import AppLayout from '@/components/app/AppLayout.vue'
 import AppLoading from '@/components/app/AppLoading.vue'
@@ -17,8 +17,12 @@ const general = useGeneralStore()
 const auth = useAuthStore()
 
 const loading = ref(true)
+
 const isLogin = computed(() => route.name === LoginView.name)
 const isUnavailable = computed(() => route.name === UnavailableSiteView.name)
+const isNotFound = computed(() => route.name === NotFoundView.name)
+
+const canUseLayout = computed(() => !(isLogin.value || isUnavailable.value || isNotFound.value))
 
 onBeforeMount(async () => {
   loading.value = true
@@ -35,7 +39,7 @@ onBeforeMount(async () => {
 
 <template>
   <AppLoading :loading="loading">
-    <AppLayout v-if="!(isLogin || isUnavailable)">
+    <AppLayout v-if="canUseLayout">
       <RouterView />
     </AppLayout>
 
